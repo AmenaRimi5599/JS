3cb9e01f32a3b57332238a882e5fbd7bc040d56b
Rimjhim <amenarimi5@gmail.com>
2022-06-26 21:22:38 +0600

    increment and decrement button add

--- counting passenger/index.js
+++
@@ -1,4 +0,0 @@
-//document.getElementById("count").innerText = 5;
-function increment() {
-  console.log("button was clicked!!");
-}
---
+++ countingpassenger/index.js
@@ -0,0 +1,19 @@
+//document.getElementById("count").innerText = 5;
+// function increment() {
+//   console.log("button was clicked!!");
+// }
+let count = 0;
+
+function increment() {
+  count = count + 1;
+  document.getElementById("count-el").innerText = count;
+}
+
+function decrement() {
+  if (count != 0) {
+    count = count - 1;
+    document.getElementById("count-el").innerText = count;
+  } else {
+    document.getElementById("count-el").innerText = "sorry";
+  }
+}
